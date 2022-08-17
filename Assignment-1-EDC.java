d7e16c1cc764b6c1eddbf1cdf006437cadb77c96
a1781637 <a1781637@student.adelaide.edu.au>
2022-08-17 16:33:35 +0930

    it is a start. created a class

--- Assignment-1-EDC.java
+++
@@ -1,5 +0,0 @@
-// assignment 1 edc
-
-// sorry i just started
-
-System.out.println("Hello World");
--- RegexEngine.java
+++ RegexEngine.java
@@ -1,5 +1,25 @@
+import java.util.List;
+import java.util.Scanner;
+
 // assignment 1 edc
-
 // sorry i just started
+  class Main {
 
-System.out.println("Hello World");
+    String name;
+    Boolean initial;
+    Boolean accept;
+    List<String> inpuStrings;
+    List<String> nextstate;
+    List<String> Epsilonnnn;
+    public static void main(String[] args) {
+      Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+      System.out.println("whats your input");
+  
+      String userName = myObj.nextLine();  // Read user input
+      System.out.println("input is: " + userName);  // Output user input
+    }
+
+    public static void main(){
+
+    }
+  }
