784743341d03b1e44d2df94e7f2024216b083d67
a1781637 <a1781637@student.adelaide.edu.au>
2022-08-26 15:39:25 +0930

    i think i did something

--- RegexEngine.java
+++ RegexEngine.java
@@ -16,13 +16,27 @@ import java.util.Scanner;
     // public static void process();
 
     public static void main(String[] args) {
-      Scanner name = new Scanner(System.in);  // Create a Scanner object
-      //System.out.println("whats your input");
-  
-      String userName = name.nextLine();  // Read user input
-      //System.out.println(userName);  // Output user input
 
-      System.out.println("true");
+      RegexEngine regex = new RegexEngine();
+
+      Scanner input = new Scanner(System.in);  // Create a Scanner object
+  
+      String moreinp = input.nextLine();  // Read user input
+
+      if (moreinp != ''){
+        regex.initial = true ;
+      }
+
+      regex.accept = true ;
+      
+      // make sure there is a character on either side
+      // make sure it doesnt take in any random shit
+
+      // make the NFA based on the command
+      
+      // while NFA is true, keep going until expression ends 
+
+      System.out.println(regex.accept);
       
     //   inputString.add(0, userName);
     //     System.out.println("okay sure"+userName);
