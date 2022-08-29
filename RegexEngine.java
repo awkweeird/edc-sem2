faccf39f0f3aaaefa4a47c208f707715e6c4e047
a1781637 <a1781637@student.adelaide.edu.au>
2022-08-29 21:39:21 +0930

    managed to get comparisons working and a single character test working

--- RegexEngine.java
+++ RegexEngine.java
@@ -1,47 +1,55 @@
 import java.util.List;
 import java.util.Scanner;
+import java.lang.Boolean;
 
 // assignment 1 edc
 // sorry i just started
-  class RegexEngine {
+public class RegexEngine {
 
     String name;
     Boolean initial;
     Boolean accept;
     static List<String> inputString;
     static List<String> nextstate;
-    static List<String> Epsilonnnn;
+    static List<String> Epsilon;
 
-    // public static void getInput(String[] args);
-    // public static void process();
+    public RegexEngine() {
+      initial = false ;
+      accept = false ;
+    }
 
     public static void main(String[] args) {
 
       RegexEngine regex = new RegexEngine();
-
+  
       Scanner input = new Scanner(System.in);  // Create a Scanner object
   
       String moreinp = input.nextLine();  // Read user input
+  
+      // what is moreinp
+      //System.out.println("what is moreinp?" + moreinp);
 
-      //if (moreinp != ""){
+      // testing a single character first 
+
+
+      if (moreinp.equals("a")){
         regex.initial = true ;
-      //}
+        // is this working?
+      }
 
-      regex.accept = true ;
-      
-      // make sure there is a character on either side
-      // make sure it doesnt take in any random shit
-
-      // make the NFA based on the command
-      
-      // while NFA is true, keep going until expression ends 
+      if (regex.initial){
+        regex.accept = true ;
+      }
 
       System.out.println(regex.accept);
       
-    //   inputString.add(0, userName);
-    //     System.out.println("okay sure"+userName);
+      // make sure there is a character on either side
+      // make sure it doesnt take in any random shit
+  
+      // make the NFA based on the command
+      
+      // while NFA is true, keep going until expression ends 
+    
     }
-
-
   }
 
