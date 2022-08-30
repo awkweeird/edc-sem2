4f41a97bda5b58ee71251f976f52fe89c7ef1ba8
a1781637 <a1781637@student.adelaide.edu.au>
2022-08-30 21:03:07 +0930

    finding Regex_Test.Class

--- Regex_Test.java
+++ Regex_Test.java
@@ -1,14 +1,14 @@
 import java.beans.Transient;
-import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertEquals;
 //import "RegexEngine.java" ;
 //import org.junit.Test;
 
 public class Regex_Test{
-    @Test
+    //@Test
     public static void main(String[] args) {
         RegexEngine regextest = new RegexEngine();
         regextest.input = "b";
         RegexEngine regexresult = regextest.take(regextest);
-        assertEquals(false, regexresult.accept);
+        //assertEquals(false, regexresult.accept);
       }
 }
