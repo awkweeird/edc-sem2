b23bc0d87d9b83a28e146aba1f50ad03cbeb9d0f
a1781637 <a1781637@student.adelaide.edu.au>
2022-08-30 17:29:34 +0930

    regex_test instead of *_test naming

---
+++ Regex_Test.java
@@ -0,0 +1,10 @@
+import java.beans.Transient;
+
+//import org.junit.Test;
+
+public class Regex_Test{
+    //@Test
+    public static void main(String[] args) {
+      System.out.println("Test");
+      }
+}
