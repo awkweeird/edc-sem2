e29407d46b223c4ffdc0e8a070d73a8266e72edf
a1781637 <a1781637@student.adelaide.edu.au>
2022-09-01 00:53:21 +0930

    broken fsa for now but the structure is down?

--- RegexEngine.java
+++ RegexEngine.java
@@ -6,55 +6,117 @@ import java.lang.Boolean;
 public class RegexEngine {
 
     String input;
+    String character; 
     Boolean initial;
     Boolean accept;
-    static List<String> inputString;
+    static List<String> regexString;
     static List<String> nextstate;
     static List<String> Epsilon;
 
     public RegexEngine() {
       input = null ;
+      character = null ;
       initial = false ;
       accept = false ;
     }
 
+
+    // regex input handler
     public RegexEngine take(RegexEngine a){
 
-      if ((a.input).equals("a")){
+      String reg = a.input ;
+
+      if (reg.equals("a")){
         a.initial = true ;
-        // is this working?
       } else {
         a.accept = false ; 
       }
 
       if (a.initial){
         a.accept = true ;
-      } else {
-        a.accept = false ;
+      }
+
+
+      for (int i=0;i<reg.length();i++){
+        //System.out.println("character is: " + Character.isLetter(reg.charAt(i))) ;
+        if (!Character.isLetter(reg.charAt(i))){
+          a.accept = false ;
+        }
       }
 
       return a;
+
+      // // brackets
+      // int bracket1 = (a.input).indexOf("(");
+      // int bracket2 = (a.input).indexOf(")");
+
+      // //alternation
+      // int alt = (a.input).indexOf("|");
+      // // repetition
+      // int rep = (a.input).indexOf("*");
+
+      // String reg = a.input;
+ 
+      // // Switch statement over above string
+      // switch (reg) {
+
+      // // Case 1
+      // case "one":
+
+      //     // Print statement corresponding case
+      //     System.out.println("one");
+
+      //     // break keyword terminates the
+      //     // code execution here itself
+      //     break;
+
+      // // Case 2
+      // case "two":
+
+      //     // Print statement corresponding case
+      //     System.out.println("two");
+      //     break;
+
+      // // Case 3
+      // case "three":
+
+      //     // Print statement corresponding case
+      //     System.out.println("three");
+      //     break;
+
+      // // Case 4
+      // // Default case
+      // default:
+
+      //     // Print statement corresponding case
+      //     System.out.println("no match");
+
+
       
     }
+
+    // inputted character handler
+    // public void character(String haha){
+
+
+
+    // }
 
     public static void main(String[] args) {
 
       RegexEngine regex = new RegexEngine();
     
       Scanner input = new Scanner(System.in);  // Create a Scanner object
-    
       String moreinp = input.nextLine();  // Read user input
   
       regex.input = moreinp ;
   
       regex.take(regex) ;
   
-  
-    
-        // what is moreinp
-        //System.out.println("what is moreinp?" + moreinp);
-  
-        // testing a single character first 
+      System.out.println("ready") ;
+
+      Scanner car = new Scanner(System.in);  // Create a Scanner object
+      String charinput = car.nextLine();  // Read user input
   
       System.out.println(regex.accept);
         
